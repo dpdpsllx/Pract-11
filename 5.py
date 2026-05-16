125fe9094bd43b00ca38f56d026ddf5a6a1d0e14
Alex <gamalexander1@gmail.com>
2026-05-16 10:35:31 +0700

    5.py

--- 5.py
+++ 5.py
@@ -6,8 +6,6 @@ numbers = set(range(2, n))
 #Проходим по возможным делителям
 for i in range(2, int(n ** 0.5) + 1):
     if i in numbers:
-        #Удаляем все кратные i
         numbers -= set(range(i * i, n, i))
 
-# Вывод элементв списка по отдельности
 print(*sorted(numbers))
