c1e7a0b4094e89890fd59491bbe5debd60f75603
Alex <gamalexander1@gmail.com>
2026-05-16 10:36:55 +0700

    7.py

--- 7.py
+++ 7.py
@@ -2,9 +2,7 @@ from itertools import permutations
 
 numbers = input().split()
 
-#Сортируем чтобы порядок был лексикографическим как надо
 numbers.sort()
 
-#Генерируем перестановки
 for p in permutations(numbers):
     print(*p)
